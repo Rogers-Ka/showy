3ddccfc31af490d06e22e7be5cf5d64eb5e33a90
Rogers-Ka <76101490+Rogers-Ka@users.noreply.github.com>
2020-12-17 02:27:33 +0300

    Update settings.py
    allowed hosts

--- showtime/settings.py
+++ showtime/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "6l)ne+24mnt(tjgu)vz$(lpb1f9uc+3ltrm!x48x+lbap$t%w="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
